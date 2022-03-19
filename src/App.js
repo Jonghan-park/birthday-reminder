3703d119a6596421b443ef86516998c77c707a57
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-18 20:40:07 -0600

    Fix error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
+import data from './data';
+import List from './List';
 
 function App() {
   return <h2>reminder project setup</h2>
--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
-export default
+export default[
 {
     id: 1,
     name: 'Bertie Yates',
     age: 29,
     image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsm.askmen.com%2Ft%2Faskmen_in%2Farticle%2Ff%2Ffacebook-p%2Ffacebook-profile-picture-affects-chances-of-gettin_fr3n.1200.jpg&f=1&nofb=1',
 }
+]
