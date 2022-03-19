41898c890561641455f966207904468ab42a1062
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-18 20:52:21 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,5 +4,11 @@ export default[
     name: 'Bertie Yates',
     age: 29,
     image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsm.askmen.com%2Ft%2Faskmen_in%2Farticle%2Ff%2Ffacebook-p%2Ffacebook-profile-picture-affects-chances-of-gettin_fr3n.1200.jpg&f=1&nofb=1',
+},
+{
+    id: 2,
+    name: 'Jonghan Park',
+    age: 34,
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.8Di5Xe-0ty58fzXIdk_2OQHaFj%26pid%3DApi&f=1',
 }
 ]
